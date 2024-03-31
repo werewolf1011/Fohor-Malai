f7132cee9c859431e26239ba60d9712f4b57b176
werewolf1011 <jenishshahi1011@gmail.com>
2024-03-31 22:29:34 +0545

    updated First Release of Fohor Malai web app

--- Fohor_Malai/info.py
+++ Fohor_Malai/info.py
@@ -1,6 +1,6 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "shahiyakendra66@gmail.com"
-EMAIL_HOST_PASSWORD = "dihvqraopboqyvyh"
+EMAIL_HOST_USER = "your_email@gmail.com"
+EMAIL_HOST_PASSWORD = "<your_email_host_password>" # less secureapp password
 EMAIL_PORT = 587
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
